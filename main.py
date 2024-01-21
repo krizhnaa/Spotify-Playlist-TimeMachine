529437bac3af49d25374a02905be381bdb785f51
krizhnaa <krizhnaprem@gmail.com>
2024-01-21 11:58:46 +0530

    Adding environment variables

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+from dotenv import load_dotenv, find_dotenv
 
 # user_date = input("Enter the year you want to travel back to? (YYYY-MM-DD) : ")
 user_date= "2015-08-12"
@@ -10,4 +12,14 @@ soup = BeautifulSoup(billboard_page, "html.parser")
 
 songs = soup.select("li ul li h3")
 songs_list = [ song.get_text().strip() for song in songs]
-print(songs_list)
+# print(songs_list)
+
+# Spotify Authentication
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+
+print(client_secret)
