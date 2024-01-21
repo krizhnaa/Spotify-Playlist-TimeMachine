88e4c3c6e589987dd67927d884dd1baf655c192e
krizhnaa <krizhnaprem@gmail.com>
2024-01-21 13:07:51 +0530

    Fetching the search result of one track adn getting its URl

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
 
 # user_date = input("Enter the year you want to travel back to? (YYYY-MM-DD) : ")
 user_date= "2015-08-12"
@@ -22,12 +24,46 @@ load_dotenv(dotenv_path)
 
 client_id = os.getenv("client_id")
 client_secret = os.getenv("client_secret")
+id = os.getenv("id")
 
-auth_manager = {
-    "SPOTIPY_CLIENT_ID" : client_id,
-    "SPOTIPY_CLIENT_SECRET" : client_secret,
-    "SPOTIPY_REDIRECT_URI" : "http://example.com"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=client_id,
+        client_secret=client_secret,
+        show_dialog=True,
+        cache_path="token.txt",
+        username="krizhnatester",
+    )
+)
+
+query = 'Pink Floyd'
+search_type = 'track'
+
+url = 'https://api.spotify.com/v1/search'
+
+headers = {
+    'Authorization': f'Bearer {id}'
 }
 
-sp = spotipy.Spotify(auth_manager=auth_manager)
+params = {
+    'q': query,
+    'type': search_type
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+if response.status_code == 200:
+    results = response.json()
+    items = results.get('tracks', {}).get('items', [])
+
+    if items:
+        first_track_uri = items[0]['uri']
+        print(f"Spotify URI of the first track: {first_track_uri}")
+    else:
+        print("No tracks found in the search results.")
+else:
+    print(f"Error: {response.status_code} - {response.text}")
+
 
