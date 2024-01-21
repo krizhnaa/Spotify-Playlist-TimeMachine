38bbf5057545c1e89889af1d856c8684cf54e3e5
krizhnaa <krizhnaprem@gmail.com>
2024-01-21 10:43:18 +0530

    Better code

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ response = requests.get(f"https://www.billboard.com/charts/hot-100/{user_date}")
 billboard_page = response.text
 soup = BeautifulSoup(billboard_page, "html.parser")
 
-songs = soup.find_all("h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
-
+songs = soup.select("li ul li h3")
 songs_list = [ song.get_text().strip() for song in songs]
 print(songs_list)
